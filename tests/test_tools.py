6f3c490de04ebfb3c3d0989a7a9dcc46181ae666
Andrey <a5kin@users.noreply.github.com>
2017-11-29 00:33:14 +0200

    Add check for orig/resulting file identity

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,3 +1,8 @@
+"""
+Test suite for df3 tools module.
+
+"""
+
 import unittest
 import glob
 import os
@@ -6,14 +11,32 @@ from df3tools import df3split, df3combine
 
 
 class TestDf3Tools(unittest.TestCase):
+    """
+    Tests for df3split and df3combine tools.
+
+    """
 
     def tearDown(self):
+        """
+        Clean up .tga and .df3 files after test.
+
+        """
         for filename in glob.glob("*.tga"):
             os.remove(filename)
         for filename in glob.glob("*.df3"):
             os.remove(filename)
 
     def test_full_cycle(self):
+        """
+        Test that DF3 split and combine produce same file.
+
+        """
         df3split.df3split('data/clouds.df3')
         self.assertEqual(len(glob.glob("*.tga")), 133)
         df3combine.df3combine('clouds.df3')
+        with open('data/clouds.df3', "rb") as df3_file:
+            orig_content = df3_file.read()
+        with open('clouds.df3', "rb") as df3_file:
+            combined_content = df3_file.read()
+        self.assertEqual(orig_content, combined_content,
+                         "Original file differs from combined file.")
